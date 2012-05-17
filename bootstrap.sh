4ee4c8894dbab24c9efd4eee4ca89cbac4c56957
Clinton <clinton@slightlymore.co.uk>
2012-05-17 16:56:17 +0100

    Use my own vim setup - modify bootstrap.sh to reflect this

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,7 +2,15 @@
 cd "$(dirname "$0")"
 git pull
 function doIt() {
+	git submodule init
+	git submodule update
+	cd .vim
+	git submodule init
+	git submodule update
+	cd ..
+	cp .vim/vimrc .vimrc
 	rsync --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh" --exclude "README.md" -av . ~
+	rm .vimrc
 }
 if [ "$1" == "--force" -o "$1" == "-f" ]; then
 	doIt
